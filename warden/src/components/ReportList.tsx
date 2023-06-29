06a012e246c372e0cd040c81f7e4370a61bf7e3a
Michael Simmons <michaelsimmonskc@gmail.com>
2023-06-29 12:42:41 -0500

    Added pagination

--- warden/src/components/ReportList.tsx
+++ warden/src/components/ReportList.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Report } from '../types';
+import { Pagination } from '@mantine/core';
 
 interface ReportListProps {
   reports: Report[];
@@ -7,14 +8,36 @@ interface ReportListProps {
 }
 
 const ReportList: React.FC<ReportListProps> = ({ reports, handleReportClick }) => {
+  const itemsPerPage = 10; // Hard-coded for now
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(reports.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentReports = reports.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="report-list">
-      <div id="title"> <p>Warden Reports</p></div>
-      {reports.map((report, index) => (
+      <div id="title">
+        <p id="titlep">Warden Reports {reports.length}</p>
+      </div>
+      {currentReports.map((report, index) => (
         <div key={index} onClick={() => handleReportClick(report)}>
           {report._id} - {report.reportedUser}
         </div>
       ))}
+
+      <Pagination
+        total={totalPages}
+        value={currentPage}
+        onChange={handlePageChange}
+      />
     </div>
   );
 };
