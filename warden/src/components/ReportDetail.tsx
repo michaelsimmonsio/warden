3ff174223811e43ac0f4dbfe14f9e97fc3cfb3f7
Michael Simmons <michaelsimmonskc@gmail.com>
2023-06-29 16:33:50 -0500

    pagination (incase prev. push didn't work) added right side for user info

--- warden/src/components/ReportDetail.tsx
+++ warden/src/components/ReportDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Report } from '../types';
 import { RejectButton, PunishmentButton } from './Buttons';
 import "../App.css"
+import { Accordion } from '@mantine/core';
 
 interface ReportDetailProps {
   report: Report | null;
@@ -10,26 +11,51 @@ interface ReportDetailProps {
 const ReportDetail: React.FC<ReportDetailProps> = ({ report }) => {
   if (!report) {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'right', height: '100%' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
         <div>Select a report to view details</div>
       </div>
     );
   }
 
-
   return (
     <div className="report-detail">
-      <h1>Report Information</h1>
-      <p>ID: {report._id}</p>
-      <p>UID: {report.uid}</p>
-      <p>Reported User: {report.reportedUser}</p>
-      <p>Reporter: {report.reporter}</p>
-      <p>Reason: {report.reason}</p>
-      <p>Context: {report.context}</p>
-      <p>Date: {report.date}</p>
-      <p> Status: {report.status}</p>
+      <div style={{ display: 'flex' }}>
+        {/* Left side */}
+        <div style={{ flex: 1, marginRight: '10px' }}>
+          <h1>Report Information</h1>
+          <p>ID: {report._id}</p>
+          <p>UID: {report.uid}</p>
+          <p>Reported User: {report.reportedUser}</p>
+          <p>Reporter: {report.reporter}</p>
+          <p>Reason: {report.reason}</p>
+          <p>Context: {report.context}</p>
+          <p>Date: {report.date}</p>
+          <p>Status: {report.status}</p>
+        </div>
+
+        {/* Right side */}
+        <div style={{ flex: 1, marginLeft: '10px' }}>
+          <h1>User Information</h1>
+          <p>ID: {report._id}</p>
+          <p>UID: {report.uid}</p>
+          <p>Reported User: {report.reportedUser}</p>
+          <p>Reporter: {report.reporter}</p>
+          <p>Reason: {report.reason}</p>
+          <p>Context: {report.context}</p>
+          <p>Date: {report.date}</p>
+          <p>Status: {report.status}</p>
+        </div>
+      </div>
+
+      <Accordion style={{ marginBottom: '10px', alignItems: 'center' }} defaultValue="customization">
+        <Accordion.Item value="customization">
+          <Accordion.Control>Previous Messages</Accordion.Control>
+          <Accordion.Panel>{report.context}</Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+
       {/* Add additional actions here */}
-      <div style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+      <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
         <PunishmentButton report={{
           id: report._id,
           username: report.reportedUser,
